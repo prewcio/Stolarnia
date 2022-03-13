29648ad2f3a6ae9231343e230409a9cbaa024c99
Prewcio <Prewcioyt@gmail.com>
2022-03-13 11:38:53 +0100

    Changes in Contact

--- app/Http/Controllers/Api/ContactController.php
+++ app/Http/Controllers/Api/ContactController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Mail\ContactMail;
 use App\Models\Message;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 
@@ -24,7 +25,7 @@ class ContactController extends Controller
         $msg->save();
         $id = $msg->id;
         $sent = now()->format('d.m.Y');
-        $sent_hr = now()->format(' H:i');
+        $sent_hr = now()->addHour()->format(' H:i');
         $content = [
             'id' => $id,
             'from' => $name,
@@ -35,5 +36,8 @@ class ContactController extends Controller
         ];
         Mail::to('prewcioyt@gmail.com')
             ->send(new ContactMail($content));
+        return response()->json([
+            'sent'=>1
+        ]);
     }
 }
