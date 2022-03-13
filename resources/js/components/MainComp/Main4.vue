c1470766f37dd2b8bd1d15d353cfd52575dd555b
Dominik Dąbrowski <86299305+xdomsky@users.noreply.github.com>
2022-03-13 15:33:19 +0100

    literowka xd

--- resources/js/components/MainComp/Main4.vue
+++ resources/js/components/MainComp/Main4.vue
@@ -73,7 +73,7 @@
               <img src="/img/test/zdjecie-2.jpg" alt="" />
             </div>
             <div class="product-desc-2 pt-2">
-              <h2>Na Różne</h2>
+              <h2>Narożne</h2>
             </div>
           </div>
         </swiper-slide>
@@ -96,7 +96,7 @@
               <img src="/img/testowe/main-3.jpg" alt="" />
             </div>
             <div class="product-desc-2 pt-2">
-              <h2>Meble Łaźienkowe</h2>
+              <h2>Meble Łazienkowe</h2>
             </div>
           </div>
         </swiper-slide>
