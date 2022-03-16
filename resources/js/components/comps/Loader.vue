de2e1faf1d56c135e703d36e48e1a7ec02311480
Dominik Dąbrowski <86299305+xdomsky@users.noreply.github.com>
2022-03-16 10:24:38 +0100

    loader css

--- resources/js/components/comps/Loader.vue
+++ resources/js/components/comps/Loader.vue
@@ -1,59 +1,42 @@
 <template>
-    <div class="page-loader" v-if="loading==true" id="loadPageCompo">
-        <img src="/img/loading.svg">
-    </div>
+  <div class="page-loader" v-if="loading == true" id="loadPageCompo">
+    <img src="/img/loading.svg" />
+  </div>
 </template>
 
 <script>
 export default {
-    Name: "loader",
-    data() {
-        return {
-            loading: true
-        }
+  Name: "loader",
+  data() {
+    return {
+      loading: true,
+    };
+  },
+  props: {
+    loadTime: {
+      type: Number,
+      default: 1500,
     },
-    props: {
-        loadTime: {
-            type: Number,
-            default: 1500
-        },
+  },
+  methods: {
+    loaderFunc() {
+      document.getElementById("loadPageCompo").style.opacity = "0";
+      setTimeout(() => {
+        document.getElementById("loadPageCompo").style.opacity = "1";
+        this.loading = true;
+        setTimeout(() => {
+          document.getElementById("loadPageCompo").style.opacity = "0";
+          setTimeout(() => {
+            this.loading = false;
+          }, 500);
+        }, this.loadTime);
+      }, 500);
     },
-    methods: {
-        loaderFunc(){
-            document.getElementById('loadPageCompo').style.opacity='0';
-            setTimeout(()=>{
-                document.getElementById('loadPageCompo').style.opacity='1';
-                this.loading = true;
-                setTimeout(()=>{
-                    document.getElementById('loadPageCompo').style.opacity='0';
-                    setTimeout(() => {
-                        this.loading = false;
-                    },500);
-                }, this.loadTime)
-            },500)
-        },
-    },
-    mounted() {
-        this.loaderFunc();
-    }
-}
+  },
+  mounted() {
+    this.loaderFunc();
+  },
+};
 </script>
 <style scoped>
-    #loadPageCompo{
-        opacity: 0;
-        width: 100vw;
-        height: 100vh;
-        position: fixed;
-        top:0;
-        left: 0;
-        z-index: 9999999999;
-        transition: 0.3s opacity;
-        background: rgba(0,0,0,0.8);
-    }
-    #loadPageCompo img {
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translateX(-50%) translateY(-50%);
-    }
 </style>
--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -13,6 +13,7 @@
 <link rel="stylesheet" href="{{ asset('css/realizations.css') }}">
 <link rel="stylesheet" href="{{ asset('css/about.css') }}">
 <link rel="stylesheet" href="{{ asset('css/socials.css') }}">
+<link rel="stylesheet" href="{{ asset('css/loading.css') }}">
 <link rel="shortcut icon" type="image/jpg" href="{{ asset('img/logo.png') }}">
 <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
 <title>ANMAR-MEBLE S.C</title>
