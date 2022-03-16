9e2985d43a0b7f7f6d3c96c3c936f7c58684f361
Prewcio <Prewcioyt@gmail.com>
2022-03-16 21:51:02 +0100

    ZZ

--- public/js/menu.js
+++ public/js/menu.js
@@ -80,10 +80,10 @@ function topFunction() {
 }
 
 $(document).ready(function() {
-    window.scrollTo(0,1);
-    window.scrollTo(0, 0);
     var hash = $(location).prop('hash');
     if(hash) {
+        window.scrollTo(0,1);
+        window.scrollTo(0, -1);
         var elem = document.getElementById(hash.substr(1));
         setTimeout(() => {
             window.scrollTo($(hash).offset().left, $(hash).offset().top);
